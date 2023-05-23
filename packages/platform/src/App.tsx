7d8c17b84f06ab182a80eafbd1e370bda48d2adb
zhangyaxiong <jiliangmason@163.com>
2023-05-23 21:44:13 +0800

    feat

--- packages/platform/src/App.tsx
+++ packages/platform/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import 'antd/dist/antd.css'
+
+import List from './pages/List'
+// import Maker from './pages/Maker'
+// import View from './pages/View'
+import './App.css'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<List />}></Route>
+        {/* <Route path="/shop/edit/:id" element={<Maker />}></Route>
+        <Route path="/shop/preview/:id" element={<View />}></Route> */}
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ packages/platform/src/pages/List/index.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Table, Space, Button, message } from 'antd';
+import type { ColumnsType } from 'antd/lib/table';
+import { useLiveQuery } from 'dexie-react-hooks';
+import { useNavigate } from 'react-router-dom'
+import dayjs from 'dayjs'
+import './index.less'
+
+
+interface DataType {
+  id: number;
+  title: string;
+  state: string;
+  createTime: Date;
+  memo: string;
+}
+
+const List: React.FC = () => {
+  let navigate = useNavigate();
+
+  const deleteShop = async (id: number) => {
+    // const res = await db.ShopList?.delete(id);
+    // if (!res) {
+    //   message.success('删除成功！')
+    // } else {
+    //   message.error('删除失败！')
+    // }
+  }
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: '名称',
+      dataIndex: 'title',
+      render: text => <a>{text}</a>,
+    },
+    {
+      title: '状态',
+      dataIndex: 'state',
+      render: text => <span>{text === 'normal' && '正常'}</span>,
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'createTime',
+      render: (time) => {
+        return dayjs(time).format('YYYY-MM-DD HH:mm:ss')
+      }
+    },
+    {
+      title: '备注',
+      dataIndex: 'memo',
+    },
+    {
+      title: '操作',
+      dataIndex: 'memo',
+      render: (_, record) => {
+        return (
+          <>
+            <Button
+              type='primary'
+              style={{ marginRight: '12px' }}
+              onClick={() => {
+                navigate(`/shop/edit/${record.id}`)
+              }}
+            >
+              编辑
+            </Button>
+            <Button
+              onClick={() => {
+                deleteShop(record.id)
+              }}
+            >
+              删除
+            </Button>
+          </>
+        )
+      }
+    }
+  ];
+
+  const addShop = async () => {
+    const id = ''
+    console.log('id', id)
+    navigate(`/shop/edit/${id}`)
+    // const res = await db.ShopList?.update(2, { "title" : "12131231"})
+    // console.log(res)
+  }
+
+  return (
+    <div className='container'>
+      <Button
+        type="primary"
+        style={{
+          marginBottom: '16px'
+        }}
+        onClick={addShop}>
+        新建
+      </Button>
+      <Table bordered columns={columns} dataSource={[]} />
+    </div>
+  )
+}
+
+export default List
+
